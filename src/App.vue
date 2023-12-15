c896ecaa8bb176478ef30d1754a820a4efcf99c9
Volodymyr Nerovnia <nerv@i.ua>
2023-12-15 21:31:42 +0200

    Base functional

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div>Work!</div>
+      <div>Likes:<span>{{ likes }}</span></div>
+      <div>Dislikes:<span>{{ dislikes }}</span></div>
+      <button @click="addLike">likes</button>
+    <button @click="addDislike">Dislikes</button>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  }
+}
 
 </script>
 
